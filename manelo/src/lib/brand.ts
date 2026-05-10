3789261b0e4159069d6280dea6214d3a239511c8
menachem6340-pixel <menachem6340@gmail.com>
2026-05-11 00:36:21 +0300

    Bump Menlo Codex PWA cache version

--- manelo/src/lib/brand.ts
+++ manelo/src/lib/brand.ts
@@ -4,4 +4,4 @@ export const APP_BRAND_TAGLINE = "מערכת ניהול לקבלן בניין";
 export const APP_DEFAULT_ORG_NAME = "מנלו בנייה";
 export const APP_DRIVE_ROOT_FOLDER = "מנלו בנייה";
 export const APP_PRODUCTION_URL = "https://menlo-codex.vercel.app";
-export const APP_PWA_CACHE_VERSION = "2026-05-10-2";
+export const APP_PWA_CACHE_VERSION = "2026-05-10-3";
