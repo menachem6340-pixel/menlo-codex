d28fa8415816b23912ceb19165338305fdbc5488
menachem6340-pixel <menachem6340@gmail.com>
2026-05-10 23:42:54 +0300

    Add Codex brand constants

---
+++ manelo/src/lib/brand.ts
@@ -0,0 +1,7 @@
+export const APP_BRAND_NAME = "מנלו קודקס";
+export const APP_BRAND_SHORT_NAME = "קודקס";
+export const APP_BRAND_TAGLINE = "מערכת ניהול לקבלן בניין";
+export const APP_DEFAULT_ORG_NAME = "מנלו בנייה";
+export const APP_DRIVE_ROOT_FOLDER = "מנלו בנייה";
+export const APP_PRODUCTION_URL = "https://menlo-codex.vercel.app";
+export const APP_PWA_CACHE_VERSION = "2026-05-10-2";
