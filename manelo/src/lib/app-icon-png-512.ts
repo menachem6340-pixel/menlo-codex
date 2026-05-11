8c9f9b7075f6131e7212ae2aa4c9b1558b533f8e
menachem6340-pixel <menachem6340@gmail.com>
2026-05-11 22:53:52 +0300

    Add generated 512 app icon assembly

---
+++ manelo/src/lib/app-icon-png-512.ts
@@ -0,0 +1,7 @@
+import { APP_ICON_PNG_512_PART_00 } from './app-icon-png-512-part-00';
+import { APP_ICON_PNG_512_PART_01 } from './app-icon-png-512-part-01';
+import { APP_ICON_PNG_512_PART_02 } from './app-icon-png-512-part-02';
+import { APP_ICON_PNG_512_PART_03 } from './app-icon-png-512-part-03';
+import { APP_ICON_PNG_512_PART_04 } from './app-icon-png-512-part-04';
+
+export const APP_ICON_PNG_512 = [APP_ICON_PNG_512_PART_00, APP_ICON_PNG_512_PART_01, APP_ICON_PNG_512_PART_02, APP_ICON_PNG_512_PART_03, APP_ICON_PNG_512_PART_04].join("");
